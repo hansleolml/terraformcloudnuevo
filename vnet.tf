897fa855fcd10ecfd590867beb90f689cc094666
leo <leo@MacBook-Pro-de-Hans.local>
2025-10-28 20:51:23 -0500

    sube archivos .tf

---
+++ vnet.tf
@@ -0,0 +1,13 @@
+# Create a resource group
+resource "azurerm_resource_group" "rg_01" {
+  name     = "rg-hans-tfcloud-dev-001"
+  location = "West Europe"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "vnet_01" {
+  name                = "vnet-hans-tfcloud-dev-001"
+  resource_group_name = azurerm_resource_group.rg_01.name
+  location            = azurerm_resource_group.rg_01.location
+  address_space       = ["10.10.0.0/16"]
+}
