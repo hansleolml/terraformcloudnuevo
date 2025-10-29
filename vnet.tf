ef2f2ac8517daf5ded92560c48366884eb114365
leo <leo@MacBook-Pro-de-Hans.local>
2025-10-28 21:07:50 -0500

    nuevo rg 2

--- vnet.tf
+++ vnet.tf
@@ -4,6 +4,11 @@ resource "azurerm_resource_group" "rg_01" {
   location = "West Europe"
 }
 
+resource "azurerm_resource_group" "rg_02" {
+  name     = "rg-hans-tfcloud-dev-002"
+  location = "West Europe"
+}
+
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "vnet_01" {
   name                = "vnet-hans-tfcloud-dev-001"
